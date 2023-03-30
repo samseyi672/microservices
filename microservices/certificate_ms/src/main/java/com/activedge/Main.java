bf00d37510a47d559bee1f1858bde7ae4926857f
Samson Oluwaseyi <samson.oluwaseyi@ubagroup.com>
2023-03-30 10:46:21 +0100

    Annotation added to securityms main and certificatems main commit

--- microservices/certificate_ms/src/main/java/com/activedge/Main.java
+++ microservices/certificate_ms/src/main/java/com/activedge/Main.java
@@ -1,7 +1,16 @@
 package com.activedge;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+@EnableEurekaClient
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class,args) ;
     }
 }
--- microservices/security_ms/src/main/java/com/activedge/Main.java
+++ microservices/security_ms/src/main/java/com/activedge/Main.java
@@ -1,7 +1,15 @@
 package com.activedge;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableEurekaClient
+@EnableSwagger2
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class,args) ;
     }
 }
